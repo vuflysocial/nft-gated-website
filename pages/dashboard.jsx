1be53bdcbdde16f3ca5064ecae3108f2bef84700
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-03-18 05:05:17 -0700

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
       <h1>Dashboard</h1>
       <p>Welcome to your dashboard!</p>
       <div>
-        <p>Download Lik Melo from Google Drive:</p>
+        <p>Download Like Melo from Google Drive:</p>
         <a href="https://drive.google.com/drive/folders/1D86x5zhlhjL0WPkguLZMGvcPmDRfY6US?usp=sharing" target="_blank" rel="noopener noreferrer">
           <button style={{borderRadius: "5px"}}>Download from Google Drive</button>
         </a>

8bbb5bf99a6b0ee411983baf9c07e896182ec3e9
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-03-18 03:34:21 -0700

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 
 export default function Dashboard() {
+  const downloadPDF = () => {
+    window.open("./downloads/likemelo.pdf");
+  };
+
+  const downloadVideo = () => {
+    window.open("./downloads/likemelo.mp4");
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
       <p>Welcome to your dashboard!</p>
+      <div>
+        <p>Download the PDF manual:</p>
+        <button onClick={downloadPDF}>Download PDF</button>
+      </div>
+      <div>
+        <p>Like Melo:</p>
+        <video width="320" height="240" controls>
+          <source src="./downloads/likemelo.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 }

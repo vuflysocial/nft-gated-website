e665f0409028c490a340c03330209657c26afbd6
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-03-18 04:50:42 -0700

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,34 +1,15 @@
 import React from "react";
 
 export default function Dashboard() {
-  const downloadPDF = () => {
-    const link = document.createElement('a');
-    link.href = '/downloads/likemelo.pdf';
-    link.download = 'likemelo.pdf';
-    link.click();
-  };
-
-  const downloadVideo = () => {
-    const link = document.createElement('a');
-    link.href = '/downloads/likemelo.mp4';
-    link.download = 'likemelo.mp4';
-    link.click();
-  };
-
   return (
     <div>
       <h1>Dashboard</h1>
       <p>Welcome to your dashboard!</p>
       <div>
-        <p>Download the PDF manual:</p>
-        <button onClick={downloadPDF}>Download PDF</button>
-      </div>
-      <div>
-        <p>Like Melo:</p>
-        <video width="320" height="240" controls>
-          <source src="/downloads/likemelo.mp4" type="video/mp4" />
-        </video>
-        <button onClick={downloadVideo}>Download video</button>
+        <p>Download Lik Melo from Google Drive:</p>
+        <a href="https://drive.google.com/drive/folders/1D86x5zhlhjL0WPkguLZMGvcPmDRfY6US?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <button style={{borderRadius: "5px"}}>Download from Google Drive</button>
+        </a>
       </div>
     </div>
   );

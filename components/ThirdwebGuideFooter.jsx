dcdfbc64c19a698619d1ed477ade4bc47c7fdfe7
meloinu <127707571+meloinu@users.noreply.github.com>
2023-03-18 02:24:53 -0700

    Update ThirdwebGuideFooter.jsx

--- components/ThirdwebGuideFooter.jsx
+++ components/ThirdwebGuideFooter.jsx
@@ -18,7 +18,7 @@ export default function ThirdwebGuideFooter() {
         role="button"
         onClick={() =>
           window.open(
-            "https://github.com/thirdweb-example/nft-gated-website",
+            "https://meloinu.netlify.app",
             "_blank"
           )
         }
@@ -39,7 +39,7 @@ export default function ThirdwebGuideFooter() {
           style={{ cursor: "pointer" }}
           onClick={() =>
             window.open(
-              "https://github.com/thirdweb-example/nft-gated-website",
+              "https://meloinu.netlify.app",
               "_blank"
             )
           }

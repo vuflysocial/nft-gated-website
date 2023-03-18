23f0c014723e8f6fa6c8eefb21f222262b95fb26
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-03-18 04:22:52 -0700

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 export default function Dashboard() {
   const buttonStyle = {
-    backgroundColor: "#0070f3",
-    color: "#fff",
-    padding: "0.5rem 1rem",
-    borderRadius: "0.25rem",
+    borderRadius: 5,
+    backgroundColor: "#4CAF50",
+    border: "none",
+    color: "white",
+    padding: "10px 20px",
+    textAlign: "center",
     textDecoration: "none",
     display: "inline-block",
-    margin: "0.5rem 0",
+    fontSize: 16,
+    margin: "4px 2px",
+    cursor: "pointer"
   };
 
   return (
@@ -16,19 +20,15 @@ export default function Dashboard() {
       <h1>Dashboard</h1>
       <p>Welcome to your dashboard!</p>
       <div>
+        <p>Download the PDF manual:</p>
         <a href="/downloads/likemelo.pdf" download="likemelo.pdf" style={buttonStyle}>
-  Download PDF
-</a>
-
+          Download PDF
+        </a>
       </div>
       <div>
         <p>Like Melo:</p>
         <video width="320" height="240" controls>
           <source src="/downloads/likemelo.mp4" type="video/mp4" />
-            <a href="/downloads/likemelo.mp4" download="likemelo.mp4" style={buttonStyle}>
-  Download Video
-</a>
-
         </video>
       </div>
     </div>

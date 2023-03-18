9ececc0838901039e8714c7ff560d417267304ec
meloinu <127707571+meloinu@users.noreply.github.com>
2023-03-18 02:25:36 -0700

    Update yourDetails.js

--- const/yourDetails.js
+++ const/yourDetails.js
@@ -1,11 +1,9 @@
 // Replace this with your contract address
-export const contractAddress = "0xB35cE7f1Be137a207e8D959B52767bEabbccE46d";
+export const contractAddress = "0x3AFc640336C05991492007798c6A9171058FE6a0";
 
 // Replace this with your domain name.
-export const domainName = "example.com";
+export const domainName = "https://like-melo-gate.vercel.app";
 
 // Minimum number of tokens required to access
 export const minimumBalance = 1;
 
-// Token ID for ERC-1155 tokens
-export const erc1155TokenId = 0;

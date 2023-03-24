acbee522afcb45b9d623934f06e69e8571294c6f
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-03-24 02:58:48 -0700

    Update dashboard.jsx

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -10,22 +10,29 @@ export default function Dashboard() {
         <a href="https://drive.google.com/drive/folders/1D86x5zhlhjL0WPkguLZMGvcPmDRfY6US?usp=sharing" target="_blank" rel="noopener noreferrer">
           <button style={{borderRadius: "5px"}}>Download from Google Drive</button>
         </a>
-     
-<div>
-<iframe src="https://cattree.herokuapp.com/rooms" width="300%" height="500px"></iframe>
-</div>
-
-<div>
-  <div style={{width: "500px", maxWidth: "100%"}}>
-    <div style={{height: "0", paddingBottom: "100%", position: "relative"}}>
-      <iframe
-        width="500"
-        height="500"
-        style={{position: "absolute", top: "0", left: "0", width: "100%", height: "100%"}}
-        frameBorder="0"
-        src="https://imgflip.com/embed/7fociq">
-      </iframe>
+      </div>
+      <div>
+        <iframe src="https://cattree.herokuapp.com/rooms" width="300%" height="500px"></iframe>
+      </div>
+      <div>
+        <div style={{width: "500px", maxWidth: "100%"}}>
+          <div style={{height: "0", paddingBottom: "100%", position: "relative"}}>
+            <iframe
+              width="500"
+              height="500"
+              style={{position: "absolute", top: "0", left: "0", width: "100%", height: "100%"}}
+              frameBorder="0"
+              src="https://imgflip.com/embed/7fociq"
+            >
+            </iframe>
+            <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", backgroundColor: "rgba(255, 255, 255, 0.8)", padding: "10px"}}>
+              <p style={{textAlign: "center", fontWeight: "bold", marginBottom: "5px"}}>Attention!</p>
+              <p style={{textAlign: "center", fontSize: "14px"}}>This content may contain flashing or moving images.</p>
+            </div>
+          </div>
+          <p><a href="https://imgflip.com/gif/7fociq">via Imgflip</a></p>
+        </div>
+      </div>
     </div>
-    <p><a href="https://imgflip.com/gif/7fociq">via Imgflip</a></p>
-  </div>
-</div>
+  );
+}
